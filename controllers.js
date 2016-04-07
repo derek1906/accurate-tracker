40441fcebcd0e01a900f18cc33ca0dc7c92a97ce
derek1906 <derek1906@gmail.com>
2016-04-07 05:24:12 -0500

    Implemented search; Added unit tests

--- controllers.js
+++ controllers.js
@@ -1,6 +1,6 @@
 function controllers(tracker){
 	tracker
-	.controller("Overall", function($scope, $mdSidenav, uiGmapGoogleMapApi){
+	.controller("Overall", function($scope, $mdSidenav, uiGmapIsReady, map, getStopDetails){
 		$scope.atLanding = true;
 	    $scope.menuClosed = true;
 	    $scope.toggleMenu = function(){
@@ -18,10 +18,92 @@ function controllers(tracker){
 			}
 	    });
 
-	    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-	    $scope.gmapsOptions = {mapTypeControl: false, streetViewControl: false};
+	    $scope.mapMeta = {
+			options: {
+				mapTypeControl: false, streetViewControl: false, 
+				scaleControl: false, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: false,
+				minZoom: 17, maxZoom: 17,
+				disableDefaultUI: true
+			},
+			center: { latitude: 45, longitude: -73 }, 
+			zoom: 17,
+			control: {},
+			background: {},
+
+			selfLocation: undefined,
+			points: [],
+
+			loaded: false,
+			actionQueue: []
+		};
+	    uiGmapIsReady.promise().then(function(){
+			$scope.mapMeta.background = {
+				bounds: new google.maps.LatLngBounds({lat: 40.0234874, lng: -88.3181197}, {lat: 40.1691264, lng: -88.1260499}),
+				fill: {color: "#000000", opacity: 0.6}
+			};
+
+			$scope.mapMeta.loaded = true;
+			
+			var queue = $scope.mapMeta.actionQueue;
+			for(var i = 0; i < queue.length; i++){
+				var entry = $scope.mapMeta.actionQueue[i];
+				map(entry.action, entry.data);
+			}
+		});
+
+	    // interacts with map
+	    $scope.$on("map", function(e, obj){
+			console.log("[Map]", obj.action);
+			var data = obj.data;
+
+			if(!$scope.mapMeta.loaded){
+				console.log("[Map]", "Map not loaded. Request stored in queue.");
+				$scope.mapMeta.actionQueue.push({
+					action: obj.action,
+					data: data
+				});
+				return;
+			}
+
+			// handle action
+			switch(obj.action){
+				case "reset":
+					$scope.mapMeta.selfLocation = undefined;
+					$scope.mapMeta.points = [];
+					break;
+
+				case "setSelfLocation":
+					$scope.mapMeta.selfLocation = data;
+					if(data)	map("moveTo", data);
+					break;
+
+				case "moveTo":
+					var gmap = $scope.mapMeta.control.getGMap();
+					var span = gmap.getBounds().toSpan();
+					gmap.panTo({
+						lat: data.latitude,
+						lng: data.longitude - span.lng() * 0.25
+					})
+					break;
+
+				case "displayPoints":
+					$scope.mapMeta.points = data.map(function(stop_id, i){
+						var stop = getStopDetails(stop_id);
+						return {
+							coords: {
+								latitude: stop.mid_lat,
+								longitude: stop.mid_lon
+							},
+							id: i
+						};
+					});
+					break;
+			}
+
+			$scope.mapMeta.control.refresh();
+	    });
 	})
-	.controller("Landing", function($scope, getNearbyStops, loadStopsDetails){
+	.controller("Landing", function($scope, getNearbyStops, loadStopsDetails, geolocation, map, $location){
 		$scope.nearbyStops = [];
 
 		loadStopsDetails();
@@ -29,14 +111,69 @@ function controllers(tracker){
 		// Get nearby stops by user's geolocation
 		getNearbyStops(10).then(function(stops){
 			$scope.nearbyStops = stops;
+
+			map("displayPoints", stops.map(function(stop){
+				return stop.stop_id;
+			}));
 		});
+
+		geolocation().then(function(latlon){
+			map("setSelfLocation", latlon);
+		});
+
+		// events
+		$scope.doSearch = function(){
+			$location.path("/search");
+		}
 	})
-	.controller("StopDetails", function($scope, $routeParams, loadStopsDetails, stop_details, getUpcomingBuses){
-		var stop_id = $routeParams.id;
+	.controller("Search", function($scope,loadStopsDetails, getAutocomplete, map){
+		// focus input
+		setTimeout(function(){
+			document.getElementById("search-input").focus();
+		});
+
+		map("reset");
+
 		loadStopsDetails()
 			.then(function(){
-				$scope.stop = stop_details.stops[stop_id];
-				console.log($scope.stop)
+				$scope.isSearching = false;
+				$scope.searchTerm = "";
+				$scope.searchResults = [];
+				$scope.performSearch = function(){
+					$scope.isSearching = true;
+					getAutocomplete($scope.searchTerm).then(function(list){
+						$scope.isSearching = false;
+						$scope.searchResults = list;
+						if(list.length){
+							map("setSelfLocation", {
+								latitude: list[0].mid_lat,
+								longitude: list[0].mid_lon
+							});
+						}else{
+							map("setSelfLocation", undefined);
+						}
+						map("displayPoints", list.map(function(stop){
+							return stop.stop_id;
+						}));
+					}, function(){
+						$scope.isSearching = false;
+					});
+				};
+			});
+	})
+	.controller("StopDetails", function($scope, $routeParams, loadStopsDetails, getStopDetails, getUpcomingBuses, map){
+		var stop_id = $routeParams.id;
+
+		map("reset");
+
+		loadStopsDetails()
+			.then(function(){
+				var stop = $scope.stop = getStopDetails(stop_id);
+				
+				map("setSelfLocation", {
+					latitude: stop.mid_lat,
+					longitude: stop.mid_lon
+				});
 			}, function(){/*error*/});
 
 		getUpcomingBuses(stop_id)
--- services.js
+++ services.js
@@ -1,5 +1,7 @@
 function services(tracker){
 	tracker
+
+	// Storage service
 	.service("storage", function(){
 		var methods = {
 			exists: function(key){
@@ -10,26 +12,54 @@ function services(tracker){
 			},
 			set: function(key, value){
 				localStorage[key] = JSON.stringify(value);
+			},
+			remove: function(key){
+				localStorage.removeItem(key);
 			}
 		};
 
 		return methods;
 	})
+
+	// Geolocation - returns a deferred promise
 	.service("geolocation", function($q){
+		var ongoingRequest = false;	// This is to deal with bullshit Firefox behavior
+		                           	//  where .getCurrentPosition only fires once
+		                           	//  even when called multiple times
+
 		return function(){
 			var deferred = $q.defer();
 
+			if(!ongoingRequest){
+				ongoingRequest = deferred.promise;
 				navigator.geolocation.getCurrentPosition(function(pos){
-					deferred.resolve({lat: pos.coords.latitude, lon: pos.coords.longitude});
+					deferred.resolve({latitude: pos.coords.latitude, longitude: pos.coords.longitude});
+					ongoingRequest = undefined;
+				}, function(){
+					deferred.reject();
+					ongoingRequest = undefined;
+				});
+			}else{
+				ongoingRequest.then(function(latlng){
+					deferred.resolve(latlng);
 				}, function(){
 					deferred.reject();
 				});
+			}
 
 			return deferred.promise;
 		};
 	})
-	.service("loadStopsDetails", function($q, stop_details, storage, getData){
-		var DATA_STORAGE_KEY = "stop_data";
+
+	// shortcut for broadcasting map events
+	.service("map", function($rootScope){
+		return function(action, data){
+			$rootScope.$broadcast("map", {action: action, data: data});
+		};
+	})
+
+	// Load stops database - Fetch from web if data is outdated or missing
+	.service("loadStopsDetails", function(DATA_STORAGE_KEY, $q, stop_details, storage, getData){
 		return function(){
 			var deferred = $q.defer();
 
@@ -41,12 +71,22 @@ function services(tracker){
 				deferred.resolve();
 			}
 			if(!storage.exists(DATA_STORAGE_KEY) || storage.get(DATA_STORAGE_KEY).last_updated - Date.now() > 7*24*60*60*1000){
-				console.log("outdated")
+				console.log("[loadStopsDetails]", "Updating database.");
 				getData("GetStops").then(
 					function(res){
 						var stops = {};
-						res.stops.forEach(function(e){
-							stops[e.stop_id] = e;
+						res.stops.forEach(function(stop){
+							stops[stop.stop_id] = stop;
+							stop.mid_lat = 0;
+							stop.mid_lon = 0;
+
+							stop.stop_points.forEach(function(stop_point){
+								stop.mid_lat += stop_point.stop_lat;
+								stop.mid_lon += stop_point.stop_lon;
+							});
+
+							stop.mid_lat /= stop.stop_points.length;
+							stop.mid_lon /= stop.stop_points.length;
 						});
 						storage.set(DATA_STORAGE_KEY, {
 							stops: stops,
@@ -66,11 +106,14 @@ function services(tracker){
 			return deferred.promise;
 		}
 	})
+
+	// Handle communications to the API server
 	.service("getData", function($q, $http){
 		var key = "77b92e5ceef640868adfc924c1735ac3";
 
 		return function(method, data){
 			var deferred = $q.defer();
+			console.log("[getData]", method, data);
 
 			if(data === undefined)	data = {};
 			data.key = key;
@@ -91,6 +134,36 @@ function services(tracker){
 			return deferred.promise;
 		};
 	})
+
+	// Handle communications to the autocomplete API server
+	.service("getAutocomplete", function($q, $http, getStopDetails){
+		return function(input){
+			var deferred = $q.defer();
+
+			if(!input){
+				deferred.resolve([]);
+			}else{
+				$http.jsonp("http://www.cumtd.com/autocomplete/Stops/v1.0/json/search", {
+					params: {
+						query: input,
+						callback: "JSON_CALLBACK"
+					}
+				})
+					.then(function(res){
+						deferred.resolve(res.data.map(function(stop){
+							return getStopDetails(stop.i);
+						}));
+					}, function(){
+						console.error("[getAutocomplete]", "JSONP failed");
+						deferred.reject();
+					});
+			}
+
+			return deferred.promise;
+		}
+	})
+
+	// Get nearby stops
 	.service("getNearbyStops", function($q, getData, geolocation){
 		return function(count){
 			var deferred = $q.defer();
@@ -100,8 +173,8 @@ function services(tracker){
 
 			geolocation()
 				.then(function(location){
-					data.lat = location.lat;
-					data.lon = location.lon;
+					data.lat = location.latitude;
+					data.lon = location.longitude;
 
 					getData("GetStopsByLatLon", data)
 						.then(function(res){
@@ -114,6 +187,8 @@ function services(tracker){
 			return deferred.promise;
 		}
 	})
+
+	// Get upcoming buses
 	.service("getUpcomingBuses", function($q, getData){
 		return function(id){
 			var deferred = $q.defer();
@@ -128,6 +203,13 @@ function services(tracker){
 				});
 
 			return deferred.promise;
+		};
+	})
+
+	// Get stop details from database
+	.service("getStopDetails", function(stop_details){
+		return function(stop_id){
+			return stop_details.stops[stop_id];
 		};
 	})
 }
