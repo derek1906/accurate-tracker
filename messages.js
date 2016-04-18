953f0219c877bb7bcb2a1e745ac35994379409d2
derek1906 <derek1906@gmail.com>
2016-04-18 01:47:39 -0500

    Update

--- messages.js
+++ messages.js
@@ -2,28 +2,38 @@ function messages(tracker){
 	var messages = {
 		"landing": {
 			"default-title": {
-				funny: ["Near as fuck."],
-				standard: "Not far away."
+				funny: ["Near as fuck.", "Damn.", "They be close."],
+				standard: "Welcome."
 			},
 			"default-subtitle": {
-				funny: ["These are the closest bus stops I could find."],
-				standard: "These are the closest bus stops relative to your position."
+				funny: ["These are the closest bus stops I could find.", "Do you know how long it took me to find these damn stops for you?", "Found some damn close bus stops. They on yo ass."],
+				standard: "Nearby bus stops will be listed below from closest to furthest."
 			}
 		},
 		"stop-details": {
 			"default-title": {
-				funny: ["Don't miss it."],
-				standard: "Don't miss it."
+				funny: ["Don't miss it.", "Which one?", "I’m. Starving."],
+				standard: "Choose a bus."
 			},
 			"default-subtitle": {
-				funny: ["Use the damn timer so you won’t miss yo bus!"],
-				standard: "Use the timer to remind yourself when a bus is arriving."
+				funny: ["You gon miss it if you don’t use the damn timer.", "Bruh... we don’t have all fucking day here.", "Need. Food. Insert. Pizza."],
+				standard: "Buses of this bus stop will be listed below."
+			}
+		},
+		"trip-details": {
+			"default-title": {
+				funny: ["So Accurate!", "Hey ladies~", "Check it out!"],
+				standard: "Arrival times."
+			},
+			"default-subtitle": {
+				funny: ["We got dem bus times & locations down to a science.", "Your bus isn’t the only thing that’s about to arrive ;)", "Bottom right to check out the fucking bus routes"],
+				standard: "And the bus route if you click the button on the bottom right."
 			}
 		}
 	};
 
 	tracker
-	.filter("msg", function(storage){
+	.filter("msg", function(storage, random){
 		var useStandard = storage.get("standard_messages");
 
 		return function(message_id, page_id){
@@ -34,7 +44,9 @@ function messages(tracker){
 			var message = page[message_id];
 			if(!message)	return "[Message Error]";
 
-			return useStandard ? message.standard : message.funny[Math.random()*message.funny.length |0];
+			console.log(random())
+
+			return useStandard ? message.standard : message.funny[random()*message.funny.length |0];
 		};
 	})
 }
