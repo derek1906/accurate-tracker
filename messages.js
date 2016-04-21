d1974c7c070e26aeec8e5168c2d81f2f3da05b3e
derek1906 <derek1906@gmail.com>
2016-04-21 04:14:24 -0500

    Async messages loading with i18n capability

--- messages.js
+++ messages.js
@@ -1,59 +1,35 @@
 function messages(tracker){
-	var messages = {
-		"Landing": {
-			"default-title": {
-				funny: ["Near as fuck.", "Damn.", "They be close."],
-				standard: "Welcome."
-			},
-			"default-subtitle": {
-				funny: ["These are the closest bus stops I could fucking find.", "Do you know how long it took me to find these damn stops for you?", "Found some damn close bus stops. They on yo ass."],
-				standard: "Nearby bus stops will be listed below from closest to furthest."
-			}
-		},
-		"StopDetails": {
-			"default-title": {
-				funny: ["Don't miss it.", "Which one?", "I’m. Starving."],
-				standard: "Choose a bus."
-			},
-			"default-subtitle": {
-				funny: ["You gon miss it if you don’t use the damn timer.", "Bruh... we don’t have all fucking day here.", "Need. Food. Insert. Pizza."],
-				standard: "Buses of this bus stop will be listed below."
-			}
-		},
-		"TripDetails": {
-			"default-title": {
-				funny: ["So Accurate!", "Hey ladies~", "Check it out!"],
-				standard: "Arrival times."
-			},
-			"default-subtitle": {
-				funny: ["We got dem bus times & locations down to a science.", "Your bus isn’t the only thing that’s about to arrive ;)", "Bottom right to check out the fucking bus routes"],
-				standard: "And the bus route if you click the button on the bottom right."
-			}
-		}
-	};
-	
+	var MESSAGE_ERROR_TEXT = "[Message Error]";
 
-	tracker.directive("msg", function(storage, random){
+	tracker.directive("msg", function(storage, random, $http){
 		var useStandard = storage.get("standard_messages");
 
-		function lookup(page_id, message_id){
-			console.log(page_id, message_id)
+		// fetch english messages
+		var messagePromise = $http.get("i18n/en.json");
+
+		function lookup(messages, page_id, message_id){
 			var page = messages[page_id];
-			if(!page)	return "[Message Error]";
+			if(!page)	return MESSAGE_ERROR_TEXT;
 
 			var message = page[message_id];
-			if(!message)	return "[Message Error]";
+			if(!message)	return MESSAGE_ERROR_TEXT;
 
 			return useStandard ? message.standard : message.funny[random()*message.funny.length |0];
 		}
 
 		return {
 			restrict: "A",
+			template: "{{message}}",
+			scope: true,
 			link: function(scope, element, attr){
 				var message_id = attr.msg;
 				var page_id = element.controller().constructor.name;
 
-				element.text(lookup(page_id, message_id));
+				messagePromise.then(function(messages){
+					scope.message = lookup(messages.data, page_id, message_id);
+				}, function(){
+					scope.message = MESSAGE_ERROR_TEXT;
+				});
 			}
 		};
 	});
